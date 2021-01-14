1822a8d0c609120d92bd0921c59a20cdd5e8123e
Philip Plant <Philip Plant>
2021-01-14 16:02:22 +0000

    Convert short form interface names

--- hpe-park-notcon.sh
+++ hpe-park-notcon.sh
@@ -2,7 +2,7 @@
 
 # use with output of 'dis int brief | inc DOWN'
 
-for i in $(cat | grep -v MAD | grep -v BE-FW | cut -f1 -d' ') ; do
+for i in $(cat | grep -v MAD | grep -v BE-FW | sed -e 's/FGE/FortyGigE/' | sed -e 's/XGE/Ten-GigabitEthernet/' | cut -f1 -d' ') ; do
   echo "int $i"
   echo " description ***PARKED***"
   echo " port access vlan 999"
